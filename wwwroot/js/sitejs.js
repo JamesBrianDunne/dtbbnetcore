f960298adacf620a675470718f259e47a9fc76fc
JamesBrianDunne <JamesBrianDunne@users.noreply.github.com>
2019-03-05 22:19:21 +0000

    Adding project files

---
+++ wwwroot/js/sitejs.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    $(document).ready(function() {
+  //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000,
+    });
+    });
+
+})();
